e406651ed4ac748d40b0999d9c6dfe9bca0b4b17
pavic989 <pavicmilos989@gmail.com>
2017-02-28 17:39:25 +0100

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,103 @@
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+  });
+});
+
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
+  });
+});
+
+function captchaCode() {
+  var Numb1, Numb2, Numb3, Numb4, Code;     
+  Numb1 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb2 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb3 = (Math.ceil(Math.random() * 10)-1).toString();
+  Numb4 = (Math.ceil(Math.random() * 10)-1).toString();
+  
+  Code = Numb1 + Numb2 + Numb3 + Numb4;
+  $("#captcha span").remove();
+  $("#captcha input").remove();
+  $("#captcha").append("<span id='code'>" + Code + "</span><input type='button' onclick='captchaCode();'>");
+}
+
+$(function() {
+  captchaCode();
+  
+  $('#contactForm').submit(function(){
+    var captchaVal = $("#code").text();
+    var captchaCode = $(".captcha").val();
+    if (captchaVal == captchaCode) {
+      $(".captcha").css({
+        "color" : "#609D29"
+      });
+    }
+    else {
+      $(".captcha").css({
+        "color" : "#CE3B46"
+      });
+    }
+    
+    var emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,10})+$/;   
+    var emailText = $(".email").val();
+    if (emailFilter.test(emailText)) {
+      $(".email").css({
+        "color" : "#609D29"
+      });
+    }
+    else {
+      $(".email").css({
+        "color" : "#CE3B46"
+      });
+    }
+    
+    var nameFilter = /^([a-zA-Z \t]{3,15})+$/;
+    var nameText = $(".name").val();
+    if (nameFilter.test(nameText)) {
+      $(".name").css({
+        "color" : "#609D29"
+      });
+    }
+    else {
+      $(".name").css({
+        "color" : "#CE3B46"
+      });
+    }
+    
+    var messageText = $(".message").val().length;
+    if (messageText > 50) {
+      $(".message").css({
+        "color" : "#609D29"
+      });
+    }
+    else {
+      $(".message").css({
+        "color" : "#CE3B46"
+      });
+    }
+    
+    if ((captchaVal !== captchaCode) || (!emailFilter.test(emailText)) || (!nameFilter.test(nameText)) || (messageText < 50)) {
+      return false;
+    }
+    if ((captchaVal == captchaCode) && (emailFilter.test(emailText)) && (nameFilter.test(nameText)) && (messageText > 50)) {
+      $("#contactForm").css("display", "none");
+      $("#form").append("<h2>Message sent!</h2>");
+      return false;
+    }
+  });       
+});
+
